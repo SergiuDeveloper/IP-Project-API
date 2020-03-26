26e98fab0844696461e83de46040e52b67378740
G2Gmycatisonfire <49059319+G2Gmycatisonfire@users.noreply.github.com>
2020-03-26 04:02:37 +0200

    [DO NOT DEPLOY] Created Modify Account Data Service
    [INDEV CODE]
    [CLEANUP REQUIRED]
    [REVIEW REQUIRED]

---
+++ Services/EndPoints/ModifyAccountData/ModifyAccountDataEndPoint.php
@@ -0,0 +1,43 @@
+<?php
+
+    /**
+     * TODO : Standardize Errors
+     * TODO : Document
+     * TODO : Apply Code Standardization where possible
+
+    require 'ModifyAccountDataFunctions.php';
+
+    if($_SERVER["REQUEST_METHOD"] != "GET"){
+        echo "REQUEST_NOT_FOUND", PHP_EOL;
+        http_response_code(400);
+        die();
+    }
+
+    $inputUsername              = $_GET['username'];
+    $inputCurrentHashedPassword = $_GET['currentHashedPassword'];
+    $inputNewHashedPassword     = $_GET['newHashedPassword'];
+    $inputNewFirstName          = $_GET['newFirstName'];
+    $inputNewLastName           = $_GET['newLastName'];
+
+    if( $inputUsername == null || $inputCurrentHashedPassword == null ){
+        echo "BAD_USERNAME_PASSWORD", PHP_EOL;
+        http_response_code(400);
+        die();
+    }
+
+    $userRow = ModifyAccountManager::fetchIDandHashedPassword($inputUsername);
+
+    $response = ModifyAccountManager::prepareResponse($userRow, $inputCurrentHashedPassword);
+
+    if($response['status'] == 'SUCCESS')
+        ModifyAccountManager::updateFieldsInDatabase(
+            $userRow['ID'],
+            $inputNewHashedPassword,
+            $inputNewFirstName,
+            $inputNewLastName
+        );
+
+    echo json_encode($response), PHP_EOL;
+    http_response_code(200);
+?>
+
---
+++ Services/EndPoints/ModifyAccountData/ModifyAccountDataFunctions.php
@@ -0,0 +1,90 @@
+<?php
+    require '../HelperClasses/DatabaseManager.php';
+
+    class ModifyAccountManager{
+        static function fetchIDandHashedPassword($username){
+            $getIDandHashedPasswordQuery = "
+                SELECT ID, Hashed_Password FROM USERS
+                    WHERE Username = :username
+            ";
+
+            DatabaseManager::Connect();
+
+            $userRowStatement = DatabaseManager::PrepareStatement($getIDandHashedPasswordQuery);
+            $userRowStatement->bindParam(":username", $username);
+            $userRowStatement->execute();
+            $userRow = $userRowStatement->fetch();
+
+            DatabaseManager::Disconnect();
+
+            return $userRow;
+        }
+
+        static function prepareResponse($userRow, $inputCurrentHashedPassword){
+            if($userRow['ID'] == null){
+                $response = [
+                    'status' => 'FAILED',
+                    'error' => 'USER_NOT_FOUND'
+                ];
+            }
+            else if($userRow['Hashed_Password'] != $inputCurrentHashedPassword){
+                $response = [
+                    'status' => 'FAILED',
+                    'error' => 'WRONG_PASSWORD'
+                ];
+            }
+            else{
+                $response = [
+                    'status' => 'SUCCESS',
+                    'error' => ''
+                ];
+            }
+
+            return $response;
+        }
+
+        static function updateHashedPasswordInDatabase($ID, $newHashedPassword){
+            $SQLUpdateStatement = "UPDATE Users SET Hashed_Password = :hashedPassword
+                WHERE :ID = Users.ID;
+            ";
+            $SQLStatement = DatabaseManager::PrepareStatement($SQLUpdateStatement);
+            $SQLStatement->bindParam(":hashedPassword", $newHashedPassword);
+            $SQLStatement->bindParam(":ID", $ID);
+            $SQLStatement->execute();
+        }
+
+        static function updateFirstNameInDatabase($ID, $newFirstName){
+            $SQLUpdateStatement = "UPDATE Users SET First_Name = :firstName
+                WHERE :ID = Users.ID;
+            ";
+            $SQLStatement = DatabaseManager::PrepareStatement($SQLUpdateStatement);
+            $SQLStatement->bindParam(":firstName", $newFirstName);
+            $SQLStatement->bindParam(":ID", $ID);
+            $SQLStatement->execute();
+        }
+
+        static function updateLastNameInDatabase($ID, $newLastName){
+            $SQLUpdateStatement = "UPDATE Users SET Last_Name = :lastName
+                WHERE :ID = Users.ID;
+            ";
+            $SQLStatement = DatabaseManager::PrepareStatement($SQLUpdateStatement);
+            $SQLStatement->bindParam(":lastName", $newLastName);
+            $SQLStatement->bindParam(":ID", $ID);
+            $SQLStatement->execute();
+
+        }
+
+        static function updateFieldsInDatabase($ID, $newHashedPassword, $newFirstName, $newLastName){
+            DatabaseManager::Connect();
+
+            if($newHashedPassword != null)
+                ModifyAccountManager::updateHashedPasswordInDatabase($ID, $newHashedPassword);
+            if($newFirstName != null)
+                ModifyAccountManager::updateFirstNameInDatabase($ID, $newFirstName);
+            if($newLastName != null)
+                ModifyAccountManager::updateLastNameInDatabase($ID, $newLastName);
+
+            DatabaseManager::Disconnect();
+        }
+    }
+?>
