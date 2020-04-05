9ef0d501c013c35c6b0316914767ef3ced5dce6a
tavi2105 <tavi2105@gmail.com>
2020-04-05 20:22:16 +0300

    Institution Deletion + fix RetrieveRoles

--- Services/EndPoints/Institution/Deletion/EndPoint.php
+++ Services/EndPoints/Institution/Deletion/EndPoint.php
@@ -6,12 +6,11 @@ require_once("../../../HelperClasses/SuccessStates.php");
 require_once("../../../HelperClasses/DatabaseManager.php");
 require_once("../../../HelperClasses/Institution/InstitutionRoles.php");
 
+CommonEndPointLogic::ValidateHTTPPOSTRequest();
 
-CommonEndPointLogic::ValidateHTTPGETRequest();
-
-$username = $_GET["username"];
-$hashedPassword = $_GET["hashedPassword"];
-$institutionName = $_GET["institutionName"];
+$username = $_POST["username"];
+$hashedPassword = $_POST["hashedPassword"];
+$institutionName = $_POST["institutionName"];
     
 if ($username == null || $hashedPassword == null || $institutionName == null) {
     $failureResponseStatus = CommonEndPointLogic::GetFailureResponseStatus("NULL_INPUT");
@@ -25,10 +24,12 @@ $responseStatus = CommonEndPointLogic::ValidateUserCredentials($username, $hashe
 
 $queryIdInstitution = "SELECT ID FROM Institutions WHERE name = :institutionName;";
 
-$queryGetRoles = "SELECT DISTINCT title FROM Institution_Roles
-WHERE Institution_ID = :institutionID;";
+$queryDeleteRoles = "DELETE FROM Institutions_Roles Where Institution_ID = :institutionID;";
+$queryDeleteMembers = "DELETE FROM Institutions_Members Where Institution_ID = :institutionID;";
+$queryDeleteAdresses = "DELETE FROM institution_addresses_list Where Institution_ID = :institutionID;";
+$queryDeleteInstitution = "DELETE FROM Institutions Where ID = :institutionID;";
 
-$institutionRoles = array();
+
 try {
     DatabaseManager::Connect();
 
@@ -47,16 +48,27 @@ try {
         die();
     }
 
-    InstitutionRoles::isUserAuthorized($username, $institutionName, InstitutionActions::ASSIGN_ROLE);
+    InstitutionRoles::isUserAuthorized($username, $institutionName, InstitutionActions::DELETE_INSTITUTION);
 
     DatabaseManager::Connect();
-    $getRoles = DatabaseManager::PrepareStatement($queryGetRoles);
-    $getRoles->bindParam(":institutionID",$institutionRow["ID"]);
-    $getRoles->execute();
+    
+    $deleteInstitution = DatabaseManager::PrepareStatement($queryDeleteRoles);
+    $deleteInstitution->bindParam(":institutionID", $institutionRow["ID"]);
+    $deleteInstitution->execute();
+    
+    $deleteInstitution = DatabaseManager::PrepareStatement($queryDeleteMembers);
+    $deleteInstitution->bindParam(":institutionID", $institutionRow["ID"]);
+    $deleteInstitution->execute();
 
-    while($getRolesRow = $getRoles->fetch(PDO::FETCH_ASSOC)){
-        array_push($institutionRoles,$getRolesRow["title"]);
-    }
+    $deleteInstitution = DatabaseManager::PrepareStatement($queryDeleteAdresses);
+    $deleteInstitution->bindParam(":institutionID", $institutionRow["ID"]);
+    $deleteInstitution->execute();
+
+    $deleteInstitution = DatabaseManager::PrepareStatement($queryDeleteInstitution);
+    $deleteInstitution->bindParam(":institutionID", $institutionRow["ID"]);
+    $deleteInstitution->execute();
+  
+    
     DatabaseManager::Disconnect();
 }
 catch (Exception $databaseException) {
@@ -68,7 +80,5 @@ catch (Exception $databaseException) {
 
 $responseSuccess = CommonEndPointLogic::GetSuccessResponseStatus();
 echo json_encode($responseSuccess), PHP_EOL;
-echo json_encode($institutionRoles), PHP_EOL;
 http_response_code(StatusCodes::OK);
-
 ?>
